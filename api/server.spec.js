56d3f19aca68c9dd9690c867dc192e80b06ac4da
RachelKolk <rach.m.obrien@gmail.com>
2019-03-08 12:25:44 -0500

    post endpoint tests

---
+++ api/server.spec.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server.js', () => {
+
+    describe('POST /games', () => {
+        it('takes in info in this format and responds with json', () => {
+            return request(server)
+              .post('/games')
+              .send({
+                  title: 'Tetris',
+                  genre: 'Console - Gameboy',
+                  releaseYear: 1984
+              })
+              .set('Accept', 'application/json')
+              .expect(200)
+        });
+
+        it('should error when title field is blank', async () => {
+            const res = await request(server)
+              .post('/games')
+              .send({
+                  title: '',
+                  genre: 'Console - SNES',
+              });
+
+            expect(res.status).toBe(422);
+        });
+
+        it('should error when genre field is blank', async () => {
+            const res = await request(server)
+              .post('/games')
+              .send({
+                  title: 'Centipede',
+                  genre: '',
+              });
+
+            expect(res.status).toBe(422);
+        });
+
+        it('should error without body'), async () => {
+            const res = await request(server).post('/games');
+
+            expect(res.status).toBe(500);
+        }
+    });
+})
